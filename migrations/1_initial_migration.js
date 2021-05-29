09a66b9ae3db7d3c5011b4e1889bf96fd352298a
Kyle Longrich <klongrich@kyles-mbp.home>
2021-05-28 22:31:54 -0400

    updating migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const MetaCardsNFT = artifacts.require("MetaCardsNft");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(MetaCardsNFT, "MetaCards", "MC");
 };
