1df41ef26fe407f2ee5434dc434da8630ae374cb
Kyle Longrich <klongrich@kyles-mbp.home>
2021-05-29 00:38:57 -0400

    adding web3 calls to front-end

--- src/components/IntakeForm/index.js
+++ src/components/IntakeForm/index.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+
 import "./IntakeForm.css";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+
+import MetaCardsABI from "./MetaCardsNft.json";
+const MetaCardNftContacts = "0x00d90b17f0A9E12A97bf330B7C718e5b92880e0B";
+
+const ONE_ETHER = 1000000000000000000;
+
+const testURI = "https//www.google.com";
 
 const IntakeForm = () => {
   const [errors, setErrors] = useState([]);
@@ -11,12 +19,24 @@ const IntakeForm = () => {
   const [twitter, setTwitter] = useState("");
   const [image, setImage] = useState(null);
 
+  const [walletAmount, setWalletAmount] = useState("");
+
+  //Need a way to generate tokenIDs
+  const [tokenID, setTokenID] = useState(147);
+  
+  //Can enter public key or pull from connected wallet with {Accounts[0]}
+  const [address, setAddress] = useState("0xb2aB0c8E2302eA6D1cF2C2bE505fbBb8816AeA41");
+  
+  //Set TokenURI as IPFS upload url and hash.
+  const [tokenURI, setTokenURI] = useState("https://www.google.com");
+
   //JSON version
   const send_as_JSON = async (e) => {
     e.preventDefault();
     let ret_JSON = { name: name, twitter: twitter, image: image };
     console.log(ret_JSON);
   };
+
   const send_to_IPFS = async (e) => {
     e.preventDefault();
 
@@ -25,7 +45,73 @@ const IntakeForm = () => {
     console.log("file:", image);
   };
 
+  async function get_tokenURI(tokenID) {
+    const  web3 = window.web3;
+    const Ethaccounts = await web3.eth.getAccounts();
+
+    const MetaCardContract = new web3.eth.Contract(
+        MetaCardsABI.abi,
+        MetaCardNftContacts
+    )
+
+    await MetaCardContract.methods.tokenURI(tokenID).call(function (error, result) {
+        console.log(result);
+    })
+  }
+
+  async function mint_nft() {
+    const  web3 = window.web3;
+    const Ethaccounts = await web3.eth.getAccounts();
+
+    const MetaCardContract = new web3.eth.Contract(
+        MetaCardsABI.abi,
+        MetaCardNftContacts
+    )
+
+    await MetaCardContract.methods
+      .mint(Ethaccounts[0], tokenID, tokenURI)
+      .send({ from: Ethaccounts[0]})
+      .once("receipt", (receipt) => {
+        console.log(receipt);
+      });
+  }
+
+  async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+      return true;
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  useEffect(async () => {
+    var wallet = await loadWeb3();
+
+    if (wallet) {
+      const web3 = window.web3;
+  
+      const accounts = await web3.eth.getAccounts();
+      const address = { account: accounts[0] }.account;
+  
+      if (address) {
+        web3.eth.getBalance(address, function (error, wei) {
+          if (!error) {
+            var balance = web3.utils.fromWei(wei, "ether");
+            setWalletAmount(balance.substring(0, 4));
+          }
+        });
+      } 
+    }
+    
+  })
+
   return (
+    <>
     <Form className="intakeForm" onSubmit={send_as_JSON}>
       <h2>Data Intake</h2>
       <div className="input-div">
@@ -68,6 +154,50 @@ const IntakeForm = () => {
         Upload
       </Button>
     </Form>
+    
+    <div Style="background-color:white; 
+                        width:300px; 
+                        height: 500px; 
+                        margin:30px;
+                        padding:15px;">
+
+            {/* <h1> Input the following Information to mint a Token </h1> */}
+
+            <h1> Enter Info: </h1>
+
+            <h2>Token ID: </h2>
+            <input
+                type="text"
+                value={tokenID}
+                onChange={e => setTokenID(e.target.value)}
+            />
+
+            <h2>Public Key:</h2>
+            <input 
+                type="text"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+            />
+
+            <h2>TokenURI:</h2>
+            <input 
+                type="text"
+                value={tokenURI}
+                onChange={e => setTokenURI(e.target.value)}
+            />
+
+            <br />
+            <br />
+
+            <button onClick={() => mint_nft()} >
+                Submit
+            </button>
+
+            <br />
+            <h2>Wallet Amount: {walletAmount}</h2>
+        </div>
+
+    </>
   );
 };
 
