d4ff92f4023c01f61bc9234264f2280c63c7ea4e
Tommy Nienchi Chen <tommynchen@gmail.com>
2021-05-28 20:48:19 -0700

    pre IPFS prep

--- src/components/IntakeForm/index.js
+++ src/components/IntakeForm/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
+
 import "./IntakeForm.css";
 
 import Form from "react-bootstrap/Form";
